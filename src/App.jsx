291e06ea0a29b7c91ebdf05b86dbbd7cccffa767
Victor Pavel Cuentas Calle <pavelcuentas@gmail.com>
2023-04-05 19:33:40 -0300

    1era preentrega

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import Menu from './components/Navbar/Navbar.jsx'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    
+      <Menu/>
+
+  )
+}
+
+export default App
